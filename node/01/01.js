0705230a12366bf5bd80b74c22e823d0d22583e7
zy <zmnaer@163.com>
2018-06-29 09:43:37 +0800

    clear up files

--- node/01/01.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var data = fs.readFileSync('01.txt');
-
-console.log(data.toString());
-
-console.log('程序结束')
--- node/01/02.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var data = fs.readFile('01.txt',(err,data)=>{
-  if(err)return console.log(err);
-  console.log(data.toString());
-});
-
-console.log('程序结束')
